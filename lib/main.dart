4a03ecc0670a8885b1f62a32a7d578fe5a0a7fc2
Ahmed <ahmedzon0598506843@gmail.com>
2023-07-08 00:58:29 +0300

    z

--- lib/main.dart
+++ lib/main.dart
@@ -15,29 +15,88 @@ class app extends StatelessWidget{
 
 
 }
+// class home extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//   return Scaffold(
+//     drawer:Drawer(),
+//     appBar: AppBar(),
+//     body: Container(
+//       height:double.infinity,
+//       width: double.infinity ,
+//     decoration: BoxDecoration(
+//       color: Colors.greenAccent,
+//         border:Border.all(color: Colors.red,width: 2),
+//        borderRadius: BorderRadius.horizontal(left: Radius.circular(0)),
+//
+//       image: DecorationImage(
+//         fit: BoxFit.contain,
+//         image: AssetImage("images/1.jpg"),
+//         repeat: ImageRepeat.repeatY
+//       )
+//     ),
+//
+//     margin: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+//     padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+//     //child:Text("zofgdfgdfgfdtyuty tyuuty tyutyuyt ytdsdfsdfdsfdsfdsfdsd",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,decoration:TextDecoration.lineThrough,backgroundColor: Colors.amber),),
+//     )
+//   );
+//   }
+
+
+
 class home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    drawer:Drawer(),
-    appBar: AppBar(),
-    body: Container(
-      height: 200,
-      width:200 ,
-    decoration: BoxDecoration(
-      color: Colors.greenAccent,
-        border:Border.all(color: Colors.red,width: 2),
-       borderRadius: BorderRadius.horizontal(left: Radius.circular(0)),
-    ),
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      body: Container(
+        // child: Card(
+        //   margin: EdgeInsets.all(20),
+        //   shape:RoundedRectangleBorder(
+        //     side: BorderSide(width: 2,color:Colors.pink, ),
+        //
+        //   ),
+        //
+        //   elevation: 15,
+        //   shadowColor: Colors.cyan,
+        // ),
+        child: Container(
+          margin: EdgeInsets.all(55),
+          child: CircleAvatar(
 
-    margin: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
-    padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-    child:Text("zofgdfgdfgfdtyuty tyuuty tyutyuyt ytdsdfsdfdsfdsfdsfdsd",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,decoration:TextDecoration.lineThrough,backgroundColor: Colors.amber),),
-    )
-  );
+            radius: 100,
+            backgroundImage: AssetImage("images/1.jpg"),
+            child: Container(
+
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.lightBlueAccent,width: 3),
+
+
+                ),
+                shadowColor: Colors.lightBlue,
+                
+              ),
+            ),
+
+          ),
+          decoration: BoxDecoration(
+
+
+          ),
+        ),
+
+
+
+      )
+
+
+    );
   }
-
-
+  
+  
 }
 
 
